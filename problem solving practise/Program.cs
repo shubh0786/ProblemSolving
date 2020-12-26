1c021ddc707e8880e107ee859b4fe7c52114b3aa
shubh0786 <shubham.singh267@gmail.com>
2020-12-27 12:24:45 +1300

    Problem1

--- problem solving practise/Program.cs
+++ problem solving practise/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+//1. Write a C# Sharp program to print Hello and your name in a separate line. Go to the editor
+//Expected Output :
+//Hello: Shubham
+
 namespace problem_solving_practise
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Shubham");
+            Console.ReadLine();
+
         }
     }
 }
