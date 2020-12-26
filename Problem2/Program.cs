9badbf2843d5d7d0884115b77ae46a12aa14d388
shubh0786 <shubham.singh267@gmail.com>
2020-12-27 12:30:50 +1300

    Problem2

--- Problem2/Program.cs
+++ Problem2/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Write a C# Sharp program to print the sum of two numbers.
+
 namespace Problem2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int a = 6;
+            int b = 5;
+            int c = a + b ;
+
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+
+            
+
         }
     }
 }
